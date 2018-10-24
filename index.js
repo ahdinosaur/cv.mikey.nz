bbf97a546b07a5cb2c6220dc7d5ae3a4bdf2e46f
Mikey <michael.williams@enspiral.com>
2018-10-24 22:04:18 +1300

    render html page (#1)

---
+++ index.js
@@ -0,0 +1,31 @@
+var vfile = require('to-vfile')
+var report = require('vfile-reporter')
+var unified = require('unified')
+var markdown = require('remark-parse')
+var mdast2hast = require('remark-rehype')
+var doc = require('rehype-document')
+var format = require('rehype-format')
+var html = require('rehype-stringify')
+var raw = require('rehype-raw')
+
+unified()
+  .use(markdown)
+  .use(mdast2hast, {
+    allowDangerousHTML: true
+  })
+  .use(raw)
+  .use(doc, {
+    title: '😺🎉 Mikey Williams ☀️🌈',
+    css: [
+      './index.css'
+    ]
+  })
+  .use(format)
+  .use(html)
+  .process(vfile.readSync('README.md'), function(err, file) {
+    console.error(report(err || file))
+    vfile.writeSync({
+      path: 'index.html',
+      contents: file.contents
+    })
+  })
