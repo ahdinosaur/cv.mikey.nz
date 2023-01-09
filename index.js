4f1c51b1a428cc98ded12ff2ca5bcd532de875ab
Michael Williams <michael.williams@enspiral.com>
2023-01-09 16:16:35 +1300

    more spring cleaning

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-import { argv } from 'node:process'
 import { write, read } from 'to-vfile'
 import { reporter } from 'vfile-reporter'
 import { unified } from 'unified'
-import markdown from 'remark-parse'
-import htmlEmojiImage from 'remark-html-emoji-image'
-import mdast2hast from 'remark-rehype'
-import doc from 'rehype-document'
-import format from 'rehype-format'
-import html from 'rehype-stringify'
-import raw from 'rehype-raw'
-
-const outputDir = argv[2]
+import remarkParse from 'remark-parse'
+import remarkEmoji from 'remark-html-emoji-image'
+import remarkRehype from 'remark-rehype'
+import rehypeDocument from 'rehype-document'
+import rehypeRaw from 'rehype-raw'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
 
 async function process ({ inputPath, outputPath, title }) {
   const inputFile = await read(inputPath)
 
   const outputFile = await unified()
-    .use(markdown)
-    .use(htmlEmojiImage, { base: '/images/' })
-    .use(mdast2hast, {
-      allowDangerousHTML: true
+    .use(remarkParse)
+    .use(remarkEmoji, { base: '/images/' })
+    .use(remarkRehype, {
+      allowDangerousHtml: true
     })
-    .use(raw)
-    .use(doc, {
+    .use(rehypeRaw)
+    .use(rehypeDocument, {
       title,
       css: [
         '/index.css'
       ]
     })
-    .use(format)
-    .use(html)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
     .process(inputFile)
 
   console.error(reporter(outputFile))
@@ -40,13 +37,15 @@ async function process ({ inputPath, outputPath, title }) {
   })
 }
 
-process({
-  inputPath: 'README.md',
-  outputPath: 'public/index.html',
-  title: '😺🎉 Mikey Williams ☀️🌈'
-})
-process({
-  inputPath: 'references/mix.md',
-  outputPath: 'public/references/mix.html',
-  title: 'Mix on Mikey Williams'
-})
+Promise.all([
+  process({
+    inputPath: 'README.md',
+    outputPath: 'public/index.html',
+    title: '😺🎉 Mikey Williams ☀️🌈'
+  }),
+  process({
+    inputPath: 'references/mix.md',
+    outputPath: 'public/references/mix.html',
+    title: 'Mix on Mikey Williams'
+  })
+])
